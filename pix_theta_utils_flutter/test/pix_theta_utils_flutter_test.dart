1312dc25e88b8a4c060ccc2641d8bf38fc219b26
kbanchi1111 <kbanchi.1111@gmail.com>
2025-10-16 11:50:37 +0900

    packaging util as flutter package

---
+++ pix_theta_utils_flutter/test/pix_theta_utils_flutter_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:pix_theta_utils_flutter/pix_theta_utils_flutter.dart';
+
+void main() {
+  test('adds one to input values', () {});
+}
