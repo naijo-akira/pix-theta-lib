5dcf5f898f7865ac5ae986a94af2102c95deb70e
kbanchi1111 <kbanchi.1111@gmail.com>
2025-10-16 16:41:26 +0900

    delete packaging as flutter package

--- pix_theta_utils_flutter/test/pix_theta_utils_flutter_test.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:pix_theta_utils_flutter/pix_theta_utils_flutter.dart';
-
-void main() {
-  test('adds one to input values', () {});
-}
